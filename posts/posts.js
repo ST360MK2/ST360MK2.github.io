5dd68be981db8eb3c3e0e3d6af4704050f075e1c
Stefan Höhe <stefan.hoehe@outlook.de>
2025-10-04 13:33:27 +0200

    Blogpost zu Differentialgleichungen in elektrischen Systemem

--- posts/posts.js
+++ posts/posts.js
@@ -32,5 +32,19 @@ const POSTS = [
     date: "2025-09-09",
     excerpt: "Wie überführt man eine Kostenfunktion in ein quadratisches Optimierungsproblem?",
     tags: ["MPC","Regelungstechnik"]
+  },
+  {
+    title: "Beschreibung von Regelkreisen im Frequenzbereich",
+    href: "/posts/Technik/Regelungstechnik/RegelungstechnikFuerIngenieure/MathematischeBehandlungVonRegelkreisen/BeschreibungVonRegelkreisenImFrequenzbereich/index.html",
+    date: "2025-09-17",
+    excerpt: "Beschreibung von Regelkreisen im Frequenzbereich mittels Frequenzgang, Ortskurve und Bodediagramm.",
+    tags: ["Regelungstechnik"]
+  },
+    {
+    title: "Differentialgleichungen für elektrische Systeme",
+    href: "/posts/Technik/Regelungstechnik/Modellbildung/Differentialgleichungen/ElektrischeSysteme/index.html",
+    date: "2025-10-04",
+    excerpt: "Herleitung der Differentialgleichungen für elektrische Systeme am Beispiel eines RLC-Schaltkreises mit zusätzlichem Widerstand.",
+    tags: ["Regelungstechnik", "Differentialgleichungen", "Modellbildung", "Elektrische Systeme"]
   }
 ];
